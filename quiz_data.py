dcdfdd1a55b4058339ddf2519eb1eaa0d6a09ecb
Riswana Nissar <riswana.n@zifornd.com>
2026-02-12 16:34:56 +0530

    Adding quiz question

--- quiz_data.py
+++ quiz_data.py
@@ -1,1 +1,24 @@
 quizzes = []
+
+def add_quiz(question, answers, correct_ans, score):
+    quiz_item = {
+        "question": question,
+        "answers": answers,
+        "correct_ans": correct_ans,
+        "score": score
+    }
+    quizzes.append(quiz_item)
+    return quizzes
+
+add_quiz(
+    "What is the capital of India?",
+    ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+    "Delhi",
+    10
+)
+add_quiz(
+    "Who wrote the Harry Potter series?",
+    ["J.K. Rowling", "Enid Blyton", "C.S. Lewis", "George Orwell"],
+    "J.K. Rowling",
+    8
+)
