55152c64a27a55a53278d65365f5fb9463fffdf9
Krishnaa Senthilkumar <krishnaa.s@zifornd.com>
2026-02-12 17:19:24 +0530

    app_state keys changed from status to app_status

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # APPLICATION STATE
-app_state = {"status": "initialized", "users": 0, "quizzes": 0}
+app_state = {"app_status": "initialized", "users": 0, "quizzes": 0}
 # Entry point of the application
 def start_app():
     print("Welcome to Quiz Engine")
