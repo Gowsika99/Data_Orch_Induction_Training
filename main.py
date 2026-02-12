b62deb5d38bb3c6eabcef5ec70b95fc9aa25d0e1
Sai Srividya Nedunuri <sai01.n@zifornd.com>
2026-02-12 16:58:24 +0530

    Update main.py to add score key to app_start and modify the start_app so that existing scores are not overwritten

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # APPLICATION STATE
-app_state = {"status": "initialized", "users": 0, "quizzes": 0}
+import Utils
+app_state = {"status": "initialized", "users": 0, "quizzes": 0, "score":0}
 def start_app():
+    
+ # Take inputs from the user (username as key, score as value)
+    user_name = input("Enter username: ").strip()
+    score = input("Enter score (integer): ")
+
+# Call the function from utils
+    updated_board = Utils.update_score(user_name,score)
+
+
+# Set app_state["scores"] to the score of the user you just updated
+    app_state["scores"] = Utils.score_board.get(user_name, 0)
+
     print("App Status:", app_state["status"])
+    print("Score:", app_state["score"])
+
+    
