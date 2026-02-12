d061bf1f395bdc1fb8ea417afd1d556a38af07f6
Ramanakishore_zifo <Ramanakishore.V@zifornd.com>
2026-02-12 15:41:22 +0530

    Implemented add_user

--- users.py
+++ users.py
@@ -1,1 +1,7 @@
+users = ["Arthur", "John", "Joel", "Deacon", "Edward"]
+
+
+def add_user(name: str) -> list[str]:
+    # Adds a user to the users list and returns the updated list
+    return users
 
