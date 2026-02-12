3b665acad31e6dbca0cf4fbdae91018aa0b7e71f
Ramanakishore_zifo <Ramanakishore.V@zifornd.com>
2026-02-12 16:37:41 +0530

    Modified add_user(name) to reject names starting with the substring adm_

--- users.py
+++ users.py
@@ -2,6 +2,17 @@ users = []
 
 
 def add_user(name: str) -> list[str]:
+    try:
+        if name.lower().startswith("adm"):
+            raise ValueError("Usernames starting with 'adm' are not allowed.")
+        else:
+            return users.append(name)
+
+    except ValueError as e:
+        print(f"Invalid username '{name}': {e}")
+
+
+
     # Adds a user to the users list and returns the updated list
-    return users
+    
 
